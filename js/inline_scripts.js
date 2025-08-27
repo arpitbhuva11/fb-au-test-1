70a00f0628739f655715770639b449e77c59b325
Arpit Bhuva <arpitbhuva.11@gmail.com>
2025-08-27 16:46:30 +0530

    init

---
+++ js/inline_scripts.js
@@ -0,0 +1,147 @@
+Object.defineProperty(window.navigator, 'userAgent', { get: function(){ return 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'; } });Object.defineProperty(window.navigator, 'vendor', { get: function(){ return ''; } });Object.defineProperty(window.navigator, 'platform', { get: function(){ return 'Android'; } });
+
+// var _Hasync= _Hasync|| [];
+// _Hasync.push(['Histats.start', '1,4957216,4,0,0,0,00010000']);
+// _Hasync.push(['Histats.fasi', '1']);
+// _Hasync.push(['Histats.track_hits', '']);
+// (function() {
+// // var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+// // hs.src = ('../s10.histats.com/js15_as.js');
+// // (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+// })();
+
+
+                  document.write(new Date().toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  })) 
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 1); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 1); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 2); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 3); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  document.write(new Date().toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  })) 
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 1); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 1); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 2); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  document.write(new Date().toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  })) 
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 1); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 1); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 2); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 3); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+                  document.write(new Date().toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  })) 
+                
+
+
+                  var d = new Date();
+                  d.setDate(d.getDate() - 3); document.write(d.toLocaleDateString(navigator.language || navigator.userLanguage, {
+                    month: 'short',
+                    day: 'numeric',
+                  }));
+                
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-1N4JCX08DZ');
+
+
+
+	window.setTimeout(function(){
+	    window.location.href = 'https://mirthbeat.com/e5749v8nfq?key=550372a797e142dfc9c438901a3dd276';
+	},300000);
